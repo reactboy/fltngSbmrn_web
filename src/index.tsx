9335d3a9bf364545e932dfc0f63a6b352a5807b5
Jun Aida <junceliz@gmail.com>
2020-08-12 23:57:15 +0900

    add: set up basic routings

--- src/index.tsx
+++ src/index.tsx
@@ -1,17 +1,31 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+// import * as serviceWorker from "./serviceWorker";
+
+import { Home, Feeds, Bookmarks, NotFound } from "./sections";
+
+const App = () => {
+    return (
+        <Router>
+            <Switch>
+                <Route exact path="/" component={Home} />
+                <Route exact path="/feeds" component={Feeds} />
+                <Route exact path="/bookmarks" component={Bookmarks} />
+                <Route path="/not-found" component={NotFound} />
+            </Switch>
+        </Router>
+    );
+};
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+    <React.StrictMode>
+        <App />
+    </React.StrictMode>,
+    document.getElementById("root")
 );
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+// serviceWorker.unregister();
---
+++ src/sections/index.ts
@@ -0,0 +1,4 @@
+export * from "./Home";
+export * from "./Feeds";
+export * from "./Bookmarks";
+export * from "./NotFound";
