cc288fc0b4563a391fb724c9f274ad85ecde4f27
Jun Aida <junceliz@gmail.com>
2020-11-17 16:55:34 +0900

    add: add header ui for login user

--- src/utils/components/AppHeader/index.tsx
+++ src/utils/components/AppHeader/index.tsx
@@ -1,8 +1,58 @@
-import React from "react";
-import { Layout, Typography, Row, Col, Button } from "antd";
+import React, { useMemo } from "react";
+import { useHistory } from "react-router-dom";
+import { Layout, Typography, Row, Col, Button, Menu, Dropdown } from "antd";
 
 const { Header } = Layout;
 const { Title } = Typography;
+
+const LoginUserMenu = () => {
+    const history = useHistory();
+    const loginUserMenu = useMemo(() => {
+        return [
+            {
+                text: "Feeds",
+                onClick: () => {
+                    history.push("/feeds");
+                },
+                danger: false,
+            },
+            {
+                text: "Bookmarks",
+                onClick: () => {
+                    history.push("/bookmarks");
+                },
+                danger: false,
+            },
+            {
+                text: "Profile",
+                onClick: () => {
+                    history.push("/profile");
+                },
+                danger: false,
+            },
+            {
+                text: "Logout",
+                onClick: () => {
+                    history.push("/top");
+                },
+                danger: true,
+            },
+        ];
+    }, []);
+    const onClickMenu = ({ key }: any) => {
+        const clickHandler = loginUserMenu[key].onClick;
+        typeof clickHandler === "function" && clickHandler();
+    };
+    return (
+        <Menu onClick={onClickMenu}>
+            {loginUserMenu.map((item, i) => (
+                <Menu.Item key={i} danger={item.danger}>
+                    {item.text}
+                </Menu.Item>
+            ))}
+        </Menu>
+    );
+};
 
 export const AppHeader = () => {
     return (
@@ -14,7 +64,12 @@ export const AppHeader = () => {
                     </Col>
                     <Col>
                         <div>
-                            <Button>menu</Button>
+                            <Dropdown
+                                placement="bottomCenter"
+                                overlay={<LoginUserMenu />}
+                            >
+                                <Button className="primary">menu</Button>
+                            </Dropdown>
                         </div>
                     </Col>
                 </Row>
